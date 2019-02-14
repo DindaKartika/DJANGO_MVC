9fd778c27646ada8e5386a8c68c82446c487a080
DindaKartika <dkartika0887@gmail.com>
2019-02-14 11:17:24 +0700

    Upload MVC

--- project_advance_views/author/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-   path('author', views.author, name='author')
-]
--- project_advance_views/author/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def author(request):
-   return render(request, 'ATA/author.html', {})
